ca950ec2b3596b99ae0342e588ee924331e6f59a
Nguyễn Văn Thanh <vanthanh23799@gmail.com>
2021-08-02 16:17:25 +0700

    update responsive blog

--- franco/assets/js/main.js
+++ franco/assets/js/main.js
@@ -4,21 +4,6 @@ $(document).ready(()=>{
     });
 });
 
-// $(document).ready(function(){
-//     var data = {
-//       dots: true,
-//       infinite: true,
-//       arrows: false,
-//       speed: 300,
-//       slidesToScroll: 1,
-//       autoplay: true,
-//       autoplaySpeed: 2000,
-//       centerMode: true,
-//       variableWidth: true,
-//     };
-//     $(".blog-slider").slick(data);
-//   });
-  
 $(document).ready(function(){
   var data = {
     dots: true,
